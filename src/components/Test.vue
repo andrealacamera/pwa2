d7ea5980d396f881aa747e07208e0103f3611713
andrealacamera <andrea.lacamera0@gmail.com>
2022-03-11 01:30:37 +0100

    standard component integration

---
+++ src/components/Test.vue
@@ -0,0 +1,11 @@
+<script setup> 
+let nome = ' created by AndreZ'
+</script>
+
+
+
+<template>
+<div class="bg-gray-800 text-yellow-400">
+  <span class="p-4"> {{ nome }}</span>
+</div>
+</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,13 @@
+<script>
+import Test from '@/components/Test.vue';
+export default {
+  components: { Test }
+}
+</script>
+
 <template>
   <div class="bg-red-900 text-red-100">
     <h1>This is an about page</h1>
+    <Test />
   </div>
 </template>
-
-<style>
-
-</style>
